082c39696f704a054ffc1bf2b961d9044e46e858
pailinglo <pailinglo@gmail.com>
2020-03-08 10:12:11 -0400

    search products by category, usage of asp-page-handler

--- OnlineStore/Pages/Products/Index.cshtml.cs
+++ OnlineStore/Pages/Products/Index.cshtml.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.AspNetCore.Mvc.Rendering;
 using OnlineStore.Models;
 using OnlineStore.Services;
 
@@ -12,14 +13,20 @@ namespace OnlineStore.Pages.Products
     public class IndexModel : PageModel
     {
         private readonly IProductRepository productRepository;
+        private readonly ICategoryRepository categoryRepository;
 
         [BindProperty(SupportsGet =true)]
         public string SearchTerm { get; set; }
-        public IEnumerable<Product> Products { get; set; }
 
-        public IndexModel(IProductRepository productRepository)
+        [BindProperty(SupportsGet = true)]
+        public int CategoryId { get; set; }
+        public IEnumerable<Product> Products { get; set; }
+        public SelectList CategoryList { get; set; }
+
+        public IndexModel(IProductRepository productRepository, ICategoryRepository categoryRepository)
         {
             this.productRepository = productRepository;
+            this.categoryRepository = categoryRepository;
         }
         public void OnGet(string searchTerm)
         {
@@ -32,6 +39,13 @@ namespace OnlineStore.Pages.Products
                 Products = productRepository.Search(searchTerm);
             }
 
+            CategoryList = new SelectList(categoryRepository.GetAllCategories(), "CategoryId", "Name");
+        }
+
+        public void OnGetBrowseByCategory(int categoryId)
+        {
+            Products = productRepository.GetProductsByCategory(categoryId);
+            CategoryList = new SelectList(categoryRepository.GetAllCategories(), "CategoryId", "Name", categoryId);
         }
     }
 }
