b39ed0ef21dfc34676c08cd554453f7419ec07fa
pailinglo <pailinglo@gmail.com>
2020-04-09 05:23:56 -0400

    Use Gmail to send e-mail, save credentials in user's secret and environment variable

--- OnlineStore.Services/SmtpMailService.cs
+++ OnlineStore.Services/SmtpMailService.cs
@@ -1,20 +1,52 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Net;
 using System.Net.Mail;
 using System.Text;
 using System.Threading.Tasks;
+using Microsoft.Extensions.Configuration;
 using OnlineStore.Models;
+using Microsoft.AspNetCore.Hosting;
 
 namespace OnlineStore.Services
 {
     public class SmtpMailService : IMailService
     {
+        private readonly IConfiguration configuration;
+        private readonly IHostingEnvironment env;
+
+        public SmtpMailService(IConfiguration configuration, IHostingEnvironment env)
+        {
+            this.configuration = configuration;
+            this.env = env;
+        }
         public async Task Send(Message message)
         {
             using(var smtp = new SmtpClient())
             {
-                smtp.DeliveryMethod = SmtpDeliveryMethod.SpecifiedPickupDirectory;
-                smtp.PickupDirectoryLocation = @"C:\Temp";
+                if (env.IsDevelopment())
+                {
+                    //for local testing:
+                    smtp.DeliveryMethod = SmtpDeliveryMethod.SpecifiedPickupDirectory;
+                    smtp.PickupDirectoryLocation = @"C:\Temp";
+                }
+                else
+                {
+                    //Use configuration values from environment variables
+                    //Production environment can't use user secrets
+                    var userName = configuration["Smtp:UserName"];
+                    var password = configuration["Smtp:Password"];
+
+                    var credential = new NetworkCredential
+                    {
+                        UserName = userName,
+                        Password = password
+                    };
+                    smtp.Credentials = credential;
+                    smtp.Host = configuration["Smtp:Host"];
+                    smtp.Port = Convert.ToInt32(configuration["Smtp:Port"]);
+                    smtp.EnableSsl = true;                    
+                }
 
                 MailMessage mailMessage = new MailMessage
                 {
